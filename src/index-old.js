2ec9e92f587f6d878e3ac93607e016e377ef658d
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2026-01-18 17:52:26 +0400

    test case updated

---
+++ src/index-old.js
@@ -0,0 +1,9 @@
+// Entry point for number to Tamil words conversion
+// Implementation will be added based on reference
+
+function numberToTamilWords(number) {
+  // TODO: Implement conversion logic
+  return "";
+}
+
+module.exports = { numberToTamilWords };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
-const { numberToTamilWords } = require('../src/index');
+const { numberToTamilWords } = require('../src/index.js');
 
 // Basic test
-console.log(numberToTamilWords(123)); // Should print Tamil words for 123
+console.log(numberToTamilWords("123")); // Should print Tamil words for 123
+console.log(numberToTamilWords("3123wevw")); // Should print Tamil words for 123
+console.log(numberToTamilWords("g2ew3")); // Should print Tamil words for 123
+console.log(numberToTamilWords("31223r3")); // Should print Tamil words for 123
+console.log(numberToTamilWords("613223")); // Should print Tamil words for 123
+console.log(numberToTamilWords("7123g")); // Should print Tamil words for 123
