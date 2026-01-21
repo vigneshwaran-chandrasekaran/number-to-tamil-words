977310fc91d98711b41376eeb53e98425b2848b6
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2026-01-21 21:32:58 +0400

    index old file delted

--- src/index-old.js
+++
@@ -1,9 +0,0 @@
-// Entry point for number to Tamil words conversion
-// Implementation will be added based on reference
-
-function numberToTamilWords(number) {
-  // TODO: Implement conversion logic
-  return "";
-}
-
-module.exports = { numberToTamilWords };
